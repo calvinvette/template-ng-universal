beb4101c19ff9f49f19a7881e672bbf897cef210
Bram Borggreve <borggreve@gmail.com>
2019-04-24 17:24:59 +0200

    Update template

--- server/main.ts
+++ server/main.ts
@@ -1,12 +1,16 @@
 import { enableProdMode } from '@angular/core';
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
+import {Logger} from '@nestjs/common';
 
 enableProdMode();
+
+const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
   app.setGlobalPrefix('api');
-  await app.listen(4000);
+  await app.listen(PORT);
+  Logger.log(`🚀 Listening on port ${PORT}`);
 }
 bootstrap();
