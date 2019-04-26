66f624644db55a7da94a5d108845055a6621f7ba
Bram Borggreve <borggreve@gmail.com>
2019-04-26 18:10:34 +0200

    Revert "Update template"
    This reverts commit beb4101c19ff9f49f19a7881e672bbf897cef210.

--- server/main.ts
+++ server/main.ts
@@ -1,16 +1,12 @@
 import { enableProdMode } from '@angular/core';
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
-import {Logger} from '@nestjs/common';
 
 enableProdMode();
-
-const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
   app.setGlobalPrefix('api');
-  await app.listen(PORT);
-  Logger.log(`🚀 Listening on port ${PORT}`);
+  await app.listen(4000);
 }
 bootstrap();
